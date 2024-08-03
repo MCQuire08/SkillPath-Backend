6e5a967ab0a9c69e7604e0db1c73718f03df09e0
Rolando Quiros <rolando.quiros@pandatechla.com>
2024-08-03 00:36:48 -0600

    API Rest - Course - Link

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -45,4 +45,72 @@ router.post("/course", async (req, res) => {
     res.status(500).json({ error: "Error al crear curso" });
   }
 });
+
+router.post("/courseLink", async (req, res) => {
+  try {
+    const { description, courseId } = req.body;
+
+    if (!description || !courseId) {
+      return res.status(400).json({ error: "Faltan datos necesarios" });
+    }
+
+    const link = await prisma.link.create({
+      data: {
+        description,
+      },
+    });
+
+    const courseLink = await prisma.courseLink.create({
+      data: {
+        courseId: parseInt(courseId, 10),
+        linkId: link.id,
+      },
+    });
+
+    const response = {
+      link,
+      courseLink,
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error("Error al crear y asociar enlace:", error);
+    res.status(500).json({ error: "Error al crear y asociar enlace" });
+  }
+});
+
+router.get('/courseLink', async (_req,res) => {
+  try{
+      const links = await prisma.link.findMany();
+      res.json(links);
+  }catch (error){
+      console.error('Error al recuperar los links:', error);
+  res.status(500).json({ error: 'Error al recuperar links' });
+  }
+});
+
+router.get("/courseLink/:courseId", async (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    if (!courseId) {
+      return res.status(400).json({ error: "ID de curso no proporcionado" });
+    }
+
+    const courseLinks = await prisma.courseLink.findMany({
+      where: {
+        courseId: parseInt(courseId, 10),
+      },
+      include: {
+        link: true,
+      },
+    });
+
+    res.json(courseLinks);
+  } catch (error) {
+    console.error("Error al recuperar los enlaces del curso:", error);
+    res.status(500).json({ error: "Error al recuperar enlaces del curso" });
+  }
+});
+
 export default router;
