d9343ae76122529222b44ca5525a58a65bb60bb0
Rolando Quiros <rolando.quiros@pandatechla.com>
2024-08-02 22:20:38 -0600

    API REST - Category - Get All - Create

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import express from 'express'
-
-import productsRoutes from './routes/products.routes.js'
-import categoriesRoutes from './routes/categories.routes.js'
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
+import categoryRoutes from './routes/category.routes.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/api', productsRoutes);
-app.use('/api', categoriesRoutes);
+app.use('/api', categoryRoutes);
 app.use('/api', authRoutes);
 app.use('/api', userRoutes)
 
---
+++ src/routes/category.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import {prisma} from '../db.js'
+
+const router = Router();
+
+router.get('/category', async (_req, res) => {
+    try {
+      const categories = await prisma.category.findMany();
+      res.json(categories);
+    } catch (error) {
+      console.error('Error al recuperar categorias:', error);
+      res.status(500).json({ error: 'Error al recuperar categorias' });
+    }
+  });
+  
+  router.post('/category', async (req, res) => {
+    try {
+      const { name } = req.body;
+  
+      const newCategory = {
+        name
+      };
+  
+      const category = await prisma.category.create({
+        data: newCategory
+      });
+  
+      res.json(category);
+    } catch (error) {
+      console.error('Error al crear la categoría:', error);
+      res.status(500).json({ error: 'Error al crear la categoría' });
+    }
+  });
+
+export default router;
